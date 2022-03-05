f97449e8542258a43233bcb6e2e68b99864498ec
blangera <Benoit.Langeraert@UGent.be>
2022-03-05 19:57:46 +0100

    benoit VS werkend

---
+++ doom_benoit_test_VS/doom_benoit_test_VS/doom_benoit_test_VS.cpp
@@ -0,0 +1,14 @@
+﻿// doom_benoit_test_VS.cpp : Defines the entry point for the application.
+//
+
+#include "doom_benoit_test_VS.h"
+#include "iostream"
+
+using namespace std;
+
+int main()
+{
+	string str = "dit is een test";
+	cout << "Hello CMake." << str << endl;
+	return 0;
+}
