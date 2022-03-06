cbb1cf87764f25a3106aaad73be3de2a05dc826d
blangera <Benoit.Langeraert@UGent.be>
2022-03-06 18:54:13 +0100

    push Doom

--- doom_benoit_test_VS/doom_benoit_test_VS/doom_benoit_test_VS.h
+++
@@ -1,8 +0,0 @@
-﻿// doom_benoit_test_VS.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
-#include <iostream>
-
-// TODO: Reference additional headers your program requires here.
