3aa33e05eb046d4e21a6e59a9f1b146d5cdf4dec
lesteyae <lennert_steyaert@hotmail.com>
2022-03-29 19:17:51 +0200

    Model introduction

---
+++ doom-quake/include/Model/Cube/CubeModel.h
@@ -0,0 +1,22 @@
+//
+// Created by Lennert Steyaert on 29/03/2022.
+//
+
+#ifndef DOOM_QUAKE_CUBEMODEL_H
+#define DOOM_QUAKE_CUBEMODEL_H
+class CubeModel {
+//public:
+//    // mesh data
+//    std::vector<Vertex> vertices;
+//    std::vector<unsigned int> indices;
+//    std::vector<Texture>      textures;
+//
+//    Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
+//    //void Draw(Shader &shader);
+//    void Draw(Shader *shader);
+//private:
+//    //  render data
+//    unsigned int VAO, VBO, EBO;
+//    void setupMesh();
+};
+#endif //DOOM_QUAKE_CUBEMODEL_H
