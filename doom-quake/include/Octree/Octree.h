c9251b99cd4e2031448e9f011b6fe5a382f16d72
lesteyae <lennert_steyaert@hotmail.com>
2022-05-15 18:09:35 +0200

    Octree cleansed

--- doom-quake/include/Octree/Octree.h
+++ doom-quake/include/Octree/Octree.h
@@ -17,25 +17,21 @@ class Octree
 public:
     Octree(BoundingBox& boundingBox, int depth);
 
-    void addModel(BaseModel* model);
-    void shoot(Ray& ray, std::vector<std::unique_ptr<Hit>> & hitPoints);
-    void draw(std::vector<std::unique_ptr<Shader>> & shaders, Culling * culling, bool octreeVisible);
+    void addModel(BaseModel* model); // Add a model to the octree
+    void shoot(Ray& ray, std::vector<std::unique_ptr<Hit>> & hitPoints); // Shooting function uses searchRecursive
+    void draw(std::vector<std::unique_ptr<Shader>> & shaders, Culling * culling, bool octreeVisible);  // Draw method
 
 private:
     std::unique_ptr<Node> root;
 
+    void addModelRecursive(Node* node, BaseModel * model); // Recursive help method
     void generateChildren(Node* node);
+    bool searchRecursive(Node* node, Ray& ray, std::vector<std::unique_ptr<Hit>>& hitPoints); // Fills hitpoints with ray collisions
 
-    bool searchRecursive(Node* node, Ray& ray, std::vector<std::unique_ptr<Hit>>& hitPoints);
-
-
+    // This code comes from:
+    // https://github.com/BSVino/MathForGameDevelopers/blob/line-box-intersection/math/collision.cpp
     bool intersect(BoundingBox& b1, Ray& ray);
-
     bool intersect(BoundingBox& b1, Ray& ray,glm::vec3 & vecIntersection, float & flFraction);
-
-
-    void addModelRecursive(Node* node, BaseModel * model);
-
     bool clipLine(int d, BoundingBox &bb, const glm::vec3 &v0, const glm::vec3 &v1, float &f_low, float &f_high);
 };
 
--- doom-quake/src/Octree/Octree.cpp
+++ doom-quake/src/Octree/Octree.cpp
@@ -10,22 +10,20 @@ Octree::Octree(BoundingBox& boundingBox, int depth) {
 }
 
 void Octree::addModel(BaseModel * model) {
-    //TODO
     Node * node = this->root.get();
-    this->addModelRecursive(node,model);
+    this->addModelRecursive(node,model); // Call recursive function
 }
 
 
 void Octree::addModelRecursive(Node* node, BaseModel * model){
     if(node->depthEnd()){
-//        std::cout<< "end" << std::endl;
-        //node->models.push_back(model); // Add model
+
     } else{
         if(!node->models.empty()) {
             if (!node->hasChildren()) {
                 generateChildren(node); // generate children
 
-                // Copy models down store in nodes
+                // Copy models down  into the accompanying children
                 for(auto m : node->models){
                     std::vector<Octants> octants = node->matchChild(m->getBoundingBox());
                     for (Octants &oc: octants) {
@@ -34,19 +32,7 @@ void Octree::addModelRecursive(Node* node, BaseModel * model){
                 }
             }
 
-            // Without storing nodes
-
-//            // Copy models down
-//            for(auto m : node->models){
-//                std::vector<Octants> octants = node->matchChild(m->getBoundingBox());
-//                for (Octants &oc: octants) {
-//                    addModelRecursive(node->children[static_cast<int>(oc)].get(), m);
-//                }
-//            }
-//            node->models.clear(); // clear?
-
-            //  Add new model
-
+            // Add model into the accompanying children
             std::vector<Octants> octants = node->matchChild(model->getBoundingBox());
             for (Octants &oc: octants) {
                 addModelRecursive(node->children[static_cast<int>(oc)].get(), model);
@@ -54,8 +40,10 @@ void Octree::addModelRecursive(Node* node, BaseModel * model){
 
         } else{
             if(!node->hasChildren()){
-                //node->models.push_back(model);
+                // Node doesn't have any children and is empty, the model is added
+                // to this node
             } else{
+                // Old code  (version non storing)
                 std::vector<Octants> octants = node->matchChild(model->getBoundingBox());
                 for (Octants &oc: octants) {
                     addModelRecursive(node->children[static_cast<int>(oc)].get(), model);
@@ -68,13 +56,14 @@ void Octree::addModelRecursive(Node* node, BaseModel * model){
 
 
 void Octree::generateChildren(Node *node) {
-    // TODO
     int depth = node->getDepth();
 
+    // Calculate new dimensions
     float xDimension = node->boundingBox.dimensions.x/2;
     float yDimension = node->boundingBox.dimensions.y/2;
     float zDimension = node->boundingBox.dimensions.z/2;
 
+    // Calculate new dimensions / 2 (centre shift)
     float xDimensionHalve = node->boundingBox.dimensions.x/4;
     float yDimensionHalve = node->boundingBox.dimensions.y/4;
     float zDimensionHalve = node->boundingBox.dimensions.z/4;
@@ -151,20 +140,23 @@ void Octree::generateChildren(Node *node) {
 }
 
 
-
 void Octree::shoot(Ray& ray, std::vector<std::unique_ptr<Hit>> & hitPoints){
 
     Node * node = root.get();
 
+    // Reset all shoot variables in basemodel (this boolean prevents duplicated hitpoints)
     for(auto m : node->models){
         m->resetShoot();
     }
-    bool res = searchRecursive(node,ray,hitPoints);
+
+    bool res = searchRecursive(node,ray,hitPoints); // Search recursive hitpoints
 }
 
 bool Octree::searchRecursive(Node* node, Ray& ray, std::vector<std::unique_ptr<Hit>>& hitPoints){
     if(node->hasChildren()){
         bool res = false;
+
+        // Search for hitpoints for all children
         for(int i=0 ; i<8;i++){
             if(intersect(node->children[i]->boundingBox, ray)) {
                 res = searchRecursive(node->children[i].get(), ray,  hitPoints);
@@ -177,11 +169,11 @@ bool Octree::searchRecursive(Node* node, Ray& ray, std::vector<std::unique_ptr<H
                 if(!m->alreadyHit()){
                     BoundingBox bx = m->getBoundingBox();
                     glm::vec3 vecIntersection;
-                    float flFraction;
+                    float flFraction; //  Useless
                     bool hit = intersect(bx,ray,  vecIntersection, flFraction);
                     if(hit){
-                        hitPoints.push_back(std::make_unique<Hit>(vecIntersection,bx));
-                        m->shoot();
+                        hitPoints.push_back(std::make_unique<Hit>(vecIntersection,bx)); // Add hitpoints
+                        m->shoot(); // Set hit flag in model
                     }
                 }
             }
@@ -191,7 +183,7 @@ bool Octree::searchRecursive(Node* node, Ray& ray, std::vector<std::unique_ptr<H
 }
 
 
-
+// This code comes from:
 // https://github.com/BSVino/MathForGameDevelopers/blob/line-box-intersection/math/collision.cpp
 
 bool Octree::intersect(BoundingBox& bb1, Ray& ray){
@@ -215,6 +207,8 @@ bool Octree::intersect(BoundingBox& bb1, Ray& ray){
     return true;
 }
 
+// This code comes from:
+// https://github.com/BSVino/MathForGameDevelopers/blob/line-box-intersection/math/collision.cpp
 bool Octree::intersect(BoundingBox& bb1, Ray& ray, glm::vec3 & vecIntersection, float & flFraction){
     float f_low = 0;
     float f_high = 1;
@@ -239,28 +233,21 @@ bool Octree::intersect(BoundingBox& bb1, Ray& ray, glm::vec3 & vecIntersection, 
 
     flFraction = f_low;
 
-
     return true;
-
 }
 
 
+// This code comes from:
+// https://github.com/BSVino/MathForGameDevelopers/blob/line-box-intersection/math/collision.cpp
 
 bool Octree::clipLine(int d, BoundingBox& bb,const glm::vec3& v0, const glm::vec3& v1, float& f_low, float& f_high){
     // f_low and f_high are the results from all clipping so far. We'll write our results back out to those parameters.
-
     // f_dim_low and f_dim_high are the results we're calculating for this current dimension.
     float f_dim_low, f_dim_high;
 
     // Find the point of intersection in this dimension only as a fraction of the total vector http://youtu.be/USjbg5QXk3g?t=3m12s
-//    f_dim_low = (aabbBox.vecMin.v[d] - v0.v[d])/(v1.v[d] - v0.v[d]);
-//    f_dim_high = (aabbBox.vecMax.v[d] - v0.v[d])/(v1.v[d] - v0.v[d]);
-
       f_dim_low = ((bb.centre[d]-bb.dimensions[d]/2) - v0[d]) / (v1[d] - v0[d]);
       f_dim_high = ((bb.centre[d]+bb.dimensions[d]/2) - v0[d]) / (v1[d] - v0[d]);
-
-//    f_dim_low = (bb.vecMin.v[d] - v0.v[d])/(v1.v[d] - v0.v[d]);
-//    f_dim_high = (aabbBox.vecMax.v[d] - v0.v[d])/(v1.v[d] - v0.v[d]);
 
     // Make sure low is less than high
     if (f_dim_high < f_dim_low)
@@ -285,22 +272,24 @@ bool Octree::clipLine(int d, BoundingBox& bb,const glm::vec3& v0, const glm::vec
 }
 
 void Octree::draw(std::vector<std::unique_ptr<Shader>> & shaders, Culling* culling, bool octreeVisible) {
-    //TODO
-
     for(auto m: root->models){
         m->resetDraw();
     }
 
+    // Draw every model in culling region
+
     glEnable(GL_STENCIL_TEST);
     glStencilFunc(GL_ALWAYS, 1, 0xFF);
-    glStencilOp(GL_REPLACE, GL_REPLACE, GL_REPLACE);
-
+    glStencilOp(GL_REPLACE, GL_REPLACE, GL_REPLACE); // Mark stencil buffer
 
     root->draw(shaders, culling, octreeVisible);
 
     for(auto m: root->models){
         m->resetDraw();
     }
+
+
+    // Draw reflection (still something wrong)
 
     glStencilFunc(GL_EQUAL, 1, 0xFF);
     glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP);
@@ -309,8 +298,7 @@ void Octree::draw(std::vector<std::unique_ptr<Shader>> & shaders, Culling* culli
 
     root->drawReflection(shaders, culling);
 
-//    glStencilFunc(GL_ALWAYS, 0, 0xFF);
     glEnable(GL_DEPTH_TEST);
     glDisable(GL_STENCIL_TEST);
-    std::cout << std::endl;
+    //std::cout << std::endl;
 }
